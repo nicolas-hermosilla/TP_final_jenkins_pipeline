b133618a26c43005c38504697311e4c66323ca64
nicolas-hermosilla <51442132+nicolas-hermosilla@users.noreply.github.com>
2021-01-25 14:46:50 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type = string
+  description = "AWS region"
+  default = "eu-west-3"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Instance type"
+  default = "t2.large"
+}
